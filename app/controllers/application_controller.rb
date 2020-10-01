e1bfa0a7b76d3ebd3d709b131dc998ad083cb94f
takeru.hirofuji <guangtengj@gmail.com>
2020-10-01 00:51:31 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    privete
+    
+    def require_user_logged_in
+     unless logged_in?
+         redirect_to login_url
+     end
+    end
+    
 end
