5c0c657b4b8b4bc5eb0514761a0d59d82e1ddc66
takeru.hirofuji <guangtengj@gmail.com>
2020-10-15 12:16:03 +0000

    timeline

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -3,12 +3,12 @@ class MicropostsController < ApplicationController
   before_action :correct_user, only: [:destroy]
   
   def create
-    @micropost = current_user.microposts.build(micropot_params)
+    @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "メッセージを投稿しました"
       redirect_to root_url
     else
-      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+      @microposts = current_user.feed_microposts.order(id: :desc).page(params[:page])
       flash.now[:danger] = "メッセージの投稿に失敗しました"
       render "toppages/index"
     end
@@ -23,7 +23,7 @@ class MicropostsController < ApplicationController
   private
 
   def micropost_params
-    params.require(:micropost).permit(:cotent)
+    params.require(:micropost).permit(:content)
   end
   
   def correct_user
